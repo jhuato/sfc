51ed2338f982d4948f553e7128f98e2064009a04
jhuato <jhuato.sfc@gmail.com>
2016-07-29 08:19:50 -0400

    Create datafiles_baseR.R

---
+++ datafiles_baseR.R
@@ -0,0 +1,6 @@
+data() # This lists all the data sets R base has
+help(EuStockMarkets) # Describes the data set
+getwd() # Turns out the working directory
+View(EuStockMarkets)
+str(EuStockMarkets) # The set is time series
+plot(EuStockMarkets) # Plots all the series
